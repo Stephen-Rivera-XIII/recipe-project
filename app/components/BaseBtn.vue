92cacb2ecd5222ed260e6634650cbe52371a8e97
Stephen-Rivera-XIII <sriveraxiii@gmail.com>
2025-04-27 18:38:08 -0500

    Code complete: development. Testing next

---
+++ app/components/BaseBtn.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  defineProps<{
+    label?: string;
+    to?: string;
+  }>();
+</script>
+<template>
+  <div>
+    <NuxtLink
+      :to="to"
+      class="px-4 py-2 text-white self-start bg-dodgeroll-gold-400 rounded-md text-base lg:text-lg cursor-pointer"
+    >
+      {{ label }}
+    </NuxtLink>
+  </div>
+</template>
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -12,6 +12,17 @@
             by John Komarnicki for the
             <span class="font-bold">Build Modern Apps with Nuxt</span> course.
           </p>
+          <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
+            I, Stephen Rivera, built this application following John's course. It's using Nuxt 3, Vue 3, and
+            Tailwind CSS.
+          </p>
+          <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
+            I wanted to learn Nuxt, but I also wanted another website on which build a test automation framework, 
+            as well as practice unit testing with Vitest.
+          </p>
+          <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
+            The files for the test automation framework will be available on Github as soon as my tests are complete.
+          </p>
         </section>
       </main>
     </div>
